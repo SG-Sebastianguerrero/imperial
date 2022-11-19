169ed2f7cecef8a44c0b6fc5322f08a3c72cfae8
ricardordgz <ricardoa.rodriguez@gmail.com>
2022-11-19 00:10:13 -0500

    Se agregan cambios asociados al módulo de videollamadas y solicitud de citas

--- src/pages/AppointmentsRequest/AppointmentsRequest.js
+++ src/pages/AppointmentsRequest/AppointmentsRequest.js
@@ -4,15 +4,67 @@ import Navbar from '../../components/Navbar/Navbar';
 import {Component} from "react"
 
 class AppointmentsRequest extends Component{
+
     render(){
         return(
             <div>
                 <Navbar></Navbar>
                 <section className="internalSection medicalHistory">
-                    <h1 className="text">Solicitud de Citas</h1>
+                    <div className="filter-container">
+                        <label className="styleTitle fijar text">Solicitud de Citas</label>
+                                <div className='w-full'>
+                                    <label className="fontSubtitle">Beneficiario</label>
+                                </div>
+                                <div className='w-full pb-4'>
+                                    <select name="select" className="w-full styleBox">
+                                        <option value="value1" selected>Seleccionar</option>
+                                        <option value="value2">Maria Alvarez Lopez</option>
+                                        <option value="value3" >Geraldine Machado Alvarez</option>
+                                        <option value="value4">Carlos Cortes Cruz</option>
+                                    </select>
+                                </div>
+                                <div className='w-full'>
+                                    <label className="fontSubtitle">Especialista</label>
+                                </div>        
+                                <div className='w-full pb-4'>
+                                    <select name="select" className="w-full styleBox">
+                                        <option value="value1" selected>Seleccionar</option>
+                                        <option value="value2">Obstetra. Mario Alcantara</option>
+                                        <option value="value3">Oncologa. Ximena Zapata</option>
+                                        <option value="value4">Value 2</option>
+                                        <option value="value5">Value 3</option>
+                                    </select>
+                                </div>
+                                <div className='w-full'>
+                                    <label className="fontSubtitle">Ubicación</label>
+                                </div>
+                                <div className='w-full pb-4'>
+                                    <select name="select" className="w-full styleBox">
+                                        <option value="value1" selected>Seleccionar</option>
+                                        <option value="value2">Av. 68 Sede Principal</option>
+                                        <option value="value3">Av. Primero de Mayo</option>
+                                        <option value="value4">Calle 94</option>
+                                        <option value="value5">Suba Calle 139</option>
+                                    </select>
+                                </div>
+                            <div className='w-full'>
+                                <label className="fontSubtitle">Fecha</label>
+                            </div>
+                            <div>
+                                <input type="date" className="w-48"></input>
+                            </div>
+                            <div className="flex w-full">
+                                <div className="w-1/2 p-8">
+                                    <button className="btnForm fontButton">Cancelar</button>
+                                </div>
+                                <div className="w-1/2 p-8">
+                                    <button className="btnForm fontButton">Confirmar</button>    
+                                </div>
+                            </div>
+                        </div>
                 </section>
             </div>
-        )
+        );
     }
 }
-export default AppointmentsRequest
+export default AppointmentsRequest;
--- src/pages/VideoCalls/VideoCalls.js
+++ src/pages/VideoCalls/VideoCalls.js
@@ -9,7 +9,120 @@ class VideoCalls extends Component{
             <div>
                 <Navbar></Navbar>
                 <section className="internalSection medicalHistory">
-                    <h1 className="text">Video llamadas</h1>
+                    <div className="filter-container">
+                        <h1 className="text fontSubtitle">Videollamadas</h1>
+                        <div>
+                            <i class="fa-solid fa-arrows-rotate fa-2xl"></i>
+                        </div>
+                        <div className='w-full text-center'>
+                            <label>Esperando a que uno de tus contactos</label>
+                        </div>
+                        <div className='dark-background w-5/6 h-48'>
+                            <div className='w-full text-center'>
+                                <label className='ligth-font'>Invitar con:</label>
+                            </div>
+                            <div class="grid grid-cols-3">
+                                <div className='w-full'>
+                                    <button className="btnForm">
+                                        <div className="ml-1">
+                                            <i class="fa-solid fa-copy fa-lg space-between" ></i> Enlace
+                                        </div>
+                                    </button> 
+                                </div>
+                                <div className='w-full'>
+                                    <button className="btnForm">
+                                        <div>
+                                            <i class="fa-brands fa-google fa-2xl space-between"></i>
+                                            Google
+                                        </div>
+                                    </button> 
+                                </div>
+                                <div className='w-full'>
+                                    <button className="btnForm">
+                                        <div>
+                                            <i class="fa-brands fa-facebook fa-2xl space-between"></i>
+                                            facebook
+                                        </div>
+                                    </button> 
+                                </div>
+
+                                <div className='w-full'>
+                                    <button className="btnForm">
+                                        <div>
+                                            <i class="fa-solid fa-envelope fa-2xl space-between"></i>
+                                            Correo
+                                        </div>
+                                    </button> 
+                                </div>
+                                <div className='w-full'>
+                                    <button className="btnForm">
+                                        <div>
+                                            <i class="fa-brands fa-whatsapp fa-2xl space-between"></i>
+                                            Whatsapp
+                                        </div>
+                                    </button> 
+                                </div>
+                                <div className='w-full'>
+                                    <button className="btnForm">
+                                        <div>
+                                            <i class="fa-brands fa-facebook-messenger fa-2xl space-between"></i>
+                                            Messenger
+                                        </div>
+                                    </button> 
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-full text-center'>
+                            <label>Cuando alguno de los contactos que invitaste ingresé al enlace, la llamada empezará automáticamente.</label>
+                        </div>
+                        <div className='w-4/5 backgrounlight'>
+                            <button type="submit" className='space-between'><i class="fa fa-search"></i></button>
+                            <input type="text" placeholder="search"  className='w-5/6 borderInput text-left'/>
+                        </div>
+                        <div className='w-full'>
+                            <div class="grid grid-cols-3 dark-background">
+                                <div className='centerElement'>
+                                    <i class="fa-solid fa-user fa-lg"></i>
+                                </div>
+                                <div className='centerElement'>
+                                    <label className='ligth-font text-center'>Juan Machado</label>
+                                </div>
+                                <div>
+                                    <button className="btnForm2 fontButton centerElement">Invitar</button> 
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='w-full'>
+                            <div class="grid grid-cols-3 dark-background">
+                                <div className='centerElement'>
+                                    <i class="fa-solid fa-user fa-lg"></i>
+                                </div>
+                                <div className='centerElement'>
+                                    <label className='ligth-font text-center'>Manuel Vasquez</label>
+                                </div>
+                                <div>
+                                    <button className="btnForm2 fontButton centerElement">Invitar</button> 
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='w-full'>
+                            <div class="grid grid-cols-3 dark-background">
+                                <div className='centerElement'>
+                                    <i class="fa-solid fa-user fa-lg"></i>
+                                </div>
+                                <div className='centerElement'>
+                                    <label className='ligth-font text-center'>Sandra Torrado</label>
+                                </div>
+                                <div>
+                                    <button className="btnForm2 fontButton centerElement">Invitar</button> 
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    
                 </section>
             </div>
         )
